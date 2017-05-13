965fbe19c9bc5e330f62bdc4471f7da2a2eeb951
Artem A. Demin <Yensj@Artems-MacBook-Pro.local>
2017-05-13 11:18:27 +0300

    выгрузка тегов в шаблон

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from db_Users import db_session, User, Urls
+from query_db import crypto, check_password, query_password
+from add_and_sort import get_from_r
+import rsa
 
 engine = create_engine('sqlite:///rss.sqlite')
 
@@ -13,10 +16,16 @@ my_flask_app = Flask(__name__)
 
 @my_flask_app.route('/')
 def home():
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	else:
-		return render_template('index.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        u = User
+        us = u.query.filter(User.log_email == e_mail).first()
+        taglist = []
+        for i in us.urls_ass:
+            taglist.append(i.tag)
+            return render_template('index.html')
+        # teglist = get_from_r(str(urls.querry.filter)) # создать запрос который будет выдавать список тегов, создать словарь по содержимому
 
 
 @my_flask_app.route('/login', methods=['POST'])
@@ -24,12 +33,21 @@ def login():
     u = User
     e_mail = str(request.form['username'])
     passwd = str(request.form['password'])
-    qry_login = u.query.filter(User.log_email == e_mail, User.password_u == passwd).first()
+    password_b =  str.encode(passwd)
+    users_mail = u.query.all()  
+    keys = crypto(users_mail)
+    priv = keys[0]               # собственно полученный приватный ключ для шифрования
+    pubkey = keys[1]
+    qry_login = u.query.filter(User.log_email == e_mail).first()
     if qry_login is None:
-    	flash('No such user or invalid password')
+        flash('No such user or invalid password')
     else: 
-    	session['logged_in'] = True
-    return home()
+        if query_password(password_b, e_mail, priv) == True:
+            session['logged_in'] = True
+            return home()
+        else:
+            return home() # вывести кнопку регистрации
+    
 
 
 @my_flask_app.route("/logout", methods=['POST'])
@@ -39,5 +57,5 @@ def logout():
 
 
 if __name__ == "__main__":
-	my_flask_app.secret_key = os.urandom(20)
-	my_flask_app.run(debug=True)
+    my_flask_app.secret_key = os.urandom(20)
+    my_flask_app.run(debug=True)
